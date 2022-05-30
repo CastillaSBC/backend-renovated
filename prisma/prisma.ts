d80ad5efa9c0deb6d71caca022078347d6ef13ad
Edge <48862659+Shigetorum635@users.noreply.github.com>
2022-05-30 22:08:53 +0200

    feat: add logging

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -2,3 +2,26 @@ import { PrismaClient } from '@prisma/client'
 export const prisma = new PrismaClient()
 
 
+export const prisma = new PrismaClient({
+  log: [
+    { level: "warn", emit: "event" },
+    { level: "info", emit: "event" },
+    { level: "error", emit: "event" },
+  ],
+  errorFormat: "pretty",
+});
+
+prisma.$on("warn", (e) => {
+  console.log(`[PRISMA - Warning] ${e}`);
+});
+
+prisma.$on("info", (e) => {
+  console.log(`[PRISMA - Info] ${e}`);
+});
+
+prisma.$on("error", (e) => {
+  console.log(`[PRISMA - Error] ${e}`);
+});
+
+
+
