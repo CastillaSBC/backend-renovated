d3171755cc97134272d03eb8032be84a701253d5
Shigero Toura <ilarumbgim1@educacion.navarra.es>
2022-08-07 00:18:31 +0200

    fix: remove all dist >:(

--- dist/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./structures/server");
-const ws_1 = require("./ws/ws");
-ws_1.io.listen(server_1.server.server);
-server_1.server.init();
